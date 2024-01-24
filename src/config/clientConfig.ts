971c831befd611f3cee886ccf299854d247af161
jpsamarino <jpsamarino3@hotmail.com>
2024-01-24 16:42:36 -0300

    fix: fix problem with localstorage

--- src/config/clientConfig.ts
+++ src/config/clientConfig.ts
@@ -2,13 +2,20 @@
 import { env } from "@/config/env";
 
 // use a cached instead consult local storage every time
-let currentServerUrl = localStorage.getItem("serverUrl") || env.LOCALSERVER;
+let currentServerUrl: string | null = null;
 
 export const clientConfig = {
   getServerUrl: () => {
+    if (currentServerUrl) {
+      return currentServerUrl;
+    } else if (typeof window === "undefined") {
+      return env.LOCALSERVER;
+    }
+    currentServerUrl = localStorage.getItem("serverUrl") || env.LOCALSERVER;
     return currentServerUrl;
   },
   setServerUrl: (serverUrl: string) => {
+    debugger;
     localStorage.setItem("serverUrl", serverUrl);
     currentServerUrl = serverUrl;
   },
