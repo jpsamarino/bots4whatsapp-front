6b1f13b619d29b0c9cc913544daddd2a0ffa0982
jpsamarino <jpsamarino3@hotmail.com>
2024-01-24 16:41:55 -0300

    fix: change erro msg

--- src/screens/LoginPage/dataValidations/serverUrlValidation.ts
+++ src/screens/LoginPage/dataValidations/serverUrlValidation.ts
@@ -2,7 +2,7 @@ import { ValidationFieldInterface } from "@/utils/validationsInterface";
 
 export const validateServerUrl: ValidationFieldInterface = (currentValue) => {
   if (currentValue === "") {
-    return { isError: true, erroMessage: "Server Andress is required" };
+    return { isError: true, erroMessage: "Server andress is required" };
   }
   return { isError: false, erroMessage: "" };
 };
