eddeb12eb4989128243f1b1ed7c99ab105f6973b
jpsamarino <jpsamarino3@hotmail.com>
2024-01-23 22:16:35 -0300

    feat: initial change in toast design

--- src/components/uiShadCn/toast.tsx
+++ src/components/uiShadCn/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed p-4 top-5 right-0  z-[100] flex max-h-screen w-full flex-col-reverse max-w-[420px]",
       className
     )}
     {...props}
@@ -28,8 +28,7 @@ const toastVariants = cva(
     variants: {
       variant: {
         default: "border bg-background text-foreground",
-        destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+        destructive: "border bg-background text-foreground text-red-500",
       },
     },
     defaultVariants: {
