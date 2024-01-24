36ca1f5a2c84edc72e577730cd4ac1f9916f1ad5
jpsamarino <jpsamarino3@hotmail.com>
2024-01-24 16:43:17 -0300

    feat: add index

---
+++ src/api/auth/signIn.ts
@@ -0,0 +1,5 @@
+import { env } from "@/config/env";
+import { fetchHttpClient } from "@/api/protocols/httpClient";
+import { HttpRequest } from "@/api/protocols/httpClientInterfaces";
+
+// is necessary?
