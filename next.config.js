0b0ca0ee55499a47a76fccc7e1bce6f8d1122090
jpsamarino <jpsamarino3@hotmail.com>
2024-01-28 16:13:04 -0300

    feat: initial config to export static files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  exclude: [
+    /__tests__/,
+    /stories/,
+    /\.(spec|test)\.[jt]sx?$/,
+    /\.test\.[jt]sx?$/,
+  ],
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
 
-module.exports = nextConfig
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
+
+module.exports = nextConfig;
