dfabdf03da561579cfb54f13d48a746328577e0e
jpsamarino <jpsamarino3@hotmail.com>
2024-01-29 19:00:17 -0300

    refactor: separete icon in a diferent path

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { getClassNameButton, type buttonVariantsType } from "./Button.styles";
+import { LoadingIcon } from "../icons/LoadingIcon";
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   buttonVariantsType & {
@@ -31,22 +32,7 @@ const Button = React.forwardRef(
         ref={ref}
         {...props}
       >
-        {isLoading && size != "icon" && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-4 w-4 animate-spin"
-          >
-            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-          </svg>
-        )}
+        {isLoading && size != "icon" && <LoadingIcon />}
         {children}
       </Comp>
     );
