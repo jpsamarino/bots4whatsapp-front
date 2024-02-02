913d8d02e91c32f6e9bcc610e1b015cecb696d0b
jpsamarino <jpsamarino3@hotmail.com>
2024-02-02 17:47:40 -0300

    fix: solve problem with svg

--- src/components/icons/DashBoardIcon.tsx
+++ src/components/icons/DashBoardIcon.tsx
@@ -5,9 +5,9 @@ export const DashBoardIcon = (props: React.HTMLAttributes<SVGElement>) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="mr-2 h-4 w-4"
   >
     <rect width="7" height="7" x="3" y="3" rx="1"></rect>
