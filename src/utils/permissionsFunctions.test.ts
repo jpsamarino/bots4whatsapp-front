3c1d1ae7d3274fdf4280f83f9586ca7d9bbcb7fb
jpsamarino <jpsamarino3@hotmail.com>
2024-02-05 19:09:32 -0300

    feat: create a function to verify Acess a functionality

---
+++ src/utils/permissionsFunctions.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, test } from "@jest/globals";
+import { isAllowAcess } from "./permissionsFunctions";
+
+describe("test is function isAllowAcess", () => {
+  test("when userPermissions is empty allways is allowed", () => {
+    const userPermissions: string[] = [];
+    const requirementPermission = ["root"];
+    const isAllow = isAllowAcess(requirementPermission, userPermissions);
+    expect(isAllow).toBe(true);
+  });
+  test("when requirementPermission is empty allways is allowed", () => {
+    const userPermissions: string[] = ["root"];
+    const requirementPermission: string[] = [];
+    const isAllow = isAllowAcess(requirementPermission, userPermissions);
+    expect(isAllow).toBe(true);
+  });
+  test("when userPermissions not match with requirementPermission return false", () => {
+    const userPermissions: string[] = ["randon"];
+    const requirementPermission = ["root"];
+    const isAllow = isAllowAcess(requirementPermission, userPermissions);
+    expect(isAllow).toBe(false);
+  });
+  test("when userPermissions not have all requirementPermission return false", () => {
+    const userPermissions: string[] = ["a1"];
+    const requirementPermission = ["a1", "b1"];
+    const isAllow = isAllowAcess(requirementPermission, userPermissions);
+    expect(isAllow).toBe(false);
+  });
+  test("when userPermissions have all requirementPermission return true", () => {
+    const userPermissions: string[] = ["a1", "b1"];
+    const requirementPermission = ["a1", "b1"];
+    const isAllow = isAllowAcess(requirementPermission, userPermissions);
+    expect(isAllow).toBe(true);
+  });
+  test("when userPermissions have all requirementPermission and plus return true", () => {
+    const userPermissions: string[] = ["a1", "b1", "plus"];
+    const requirementPermission = ["a1", "b1"];
+    const isAllow = isAllowAcess(requirementPermission, userPermissions);
+    expect(isAllow).toBe(true);
+  });
+});
---
+++ src/utils/permissionsFunctions.ts
@@ -0,0 +1,22 @@
+// userPermissions empty have all access
+
+export function isAllowAcess(
+  requirementsPermissions?: string[],
+  userPermissions?: string[] | null
+) {
+  if (requirementsPermissions == undefined) {
+    return true; // there is no requirements
+  }
+  if (userPermissions == null || userPermissions == undefined) {
+    return false;
+  }
+  if (userPermissions.length == 0) {
+    return true; // userPermissions empty have all access
+  }
+  for (const requirementPermission of requirementsPermissions) {
+    if (!userPermissions.includes(requirementPermission)) {
+      return false;
+    }
+  }
+  return true;
+}
